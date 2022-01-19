a43bcf5226cb37f482099fa20f02bfb5ad308ec3
Mo2024 <mohdosama2025@gmail.com>
2022-01-19 21:52:51 +0300

    Overlay added

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// $(function () {
+//     //$('#menuleftcontent').css('width','0px');
+//     $("#openmenu").click(function () {
+//         $('#menuleftcontent').animate({
+//             width: "toggle"
+//         });
+//     });
+// });
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    $('#menuleftcontent').animate({
+        width: "toggle"
+    });
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("menuleftcontent").style.display = "none"
+}
+
