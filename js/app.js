4553bfefe4349efde916eaaf5546f0084193c8c5
Mo2024 <mohdosama2025@gmail.com>
2022-01-21 13:05:51 +0300

    Only search button is left

--- js/app.js
+++ js/app.js
@@ -7,15 +7,15 @@
 //     });
 // });
 
-function on() {
-    document.getElementById("overlay").style.display = "block";
-    $('#menuleftcontent').animate({
-        width: "toggle"
-    });
-}
+// function on() {
+//     document.getElementById("overlay").style.display = "block";
+//     $('#menuleftcontent').animate({
+//         width: "toggle"
+//     });
+// }
 
-function off() {
-    document.getElementById("overlay").style.display = "none";
-    document.getElementById("menuleftcontent").style.display = "none"
-}
+// function off() {
+//     document.getElementById("overlay").style.display = "none";
+//     document.getElementById("menuleftcontent").style.display = "none"
+// }
 
